3e2c4d8c0002ed82c91aa00b131eb4a5f9650ea7
eg4000 <eg4000@gmail.com>
2013-03-21 17:14:52 +0200

    Finish sign in

--- app/app/models/user.rb
+++ app/app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessible :name # TODO read only
 
+  before_save :create_remember_token
+
   validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
